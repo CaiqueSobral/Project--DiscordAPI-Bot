8321d7356ffcfdab6f33c713e26b9af0212be760
Caique Sobral <caiquelsobral@gmail.com>
2022-08-30 15:23:44 -0300

    FEAT: The bot can now say hello

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-import Discord from 'discord.js';
+import Discord, { GatewayIntentBits } from 'discord.js';
 import 'dotenv/config';
 
 const token = process.env.TOKEN;
+const bits = GatewayIntentBits;
+const options = [bits.Guilds, bits.GuildMessages, bits.GuildMessageReactions];
 
 const client = new Discord.Client({
-  intents: [Discord.GatewayIntentBits.Guilds],
+  intents: options,
   partials: ['CHANNEL', 'GUILD_MEMBER', 'MESSAGE', 'REACTION', 'USER'],
 });
 
 client.on('ready', (client) => {
   console.log(`The bot ${client.user.tag} is now online!`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+  message.reply('Opa, iae!');
 });
 
 client.login(token);
