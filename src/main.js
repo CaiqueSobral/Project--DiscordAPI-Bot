08c5e810dd2f537342d1f975c95c5614431021d2
Caique Sobral <caiquelsobral@gmail.com>
2022-09-02 10:18:46 -0300

    FEAT: changes for heroku

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 import { Bot } from './interfaces/bot.js';
+import http from 'http';
+
+nodeApp.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const port = process.env.PORT || 3000;
+nodeApp.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
 
 const bot = new Bot();
