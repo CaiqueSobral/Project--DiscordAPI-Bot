c5bfb8805c718b8afa8b6fcae2c31e7df9741c17
Caique Sobral <caiquelsobral@gmail.com>
2022-09-26 08:41:57 -0300

    Feat: minor changes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import { Bot } from './interfaces/bot.js';
+import express, { Router } from 'express';
 import http from 'http';
 
 const nodeApp = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Bot is online!');
 });
+
+const routes = Router();
+routes.get('/', (req, res) => res.send('API is running!'));
+
+const app = express();
+app.use('/api/v1', routes);
 
 const port = process.env.PORT || 3000;
 nodeApp.listen(port, () => {
